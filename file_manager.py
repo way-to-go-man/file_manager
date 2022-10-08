76ca02b2f360b6ecdd7e9e0cb6bf07189227eb31
Vladislav Bondarev <vlad@Vladislavs-MacBook-Pro.local>
2022-10-08 23:17:06 +0300

    commit

--- file_manager.py
+++ file_manager.py
@@ -1,24 +1,37 @@
 import os
 import shutil as sh
 
+#Создаем файл
 def create_file(name, text = None):
     with open(name, 'w', encoding = 'utf-8') as f:
         if text:
             f.write(text)
-        print('File created')
+    print(f'File {name} is created')
 
+#Считываем данные файла
+def open_file(name):
+    print(f'File {name} is opened')
+    with open(name, 'r') as f:
+        for line in f:
+            print(line)
+
+#Создаем папку
 def create_folder(name):
     try:
-        os.makedirs(name)
+        os.mkdir(name)
     except FileExistsError:
         print('This folder already exists')
+    print(f'Folder {name} created')
 
+#Удаляем файл/папку
 def delete_file(name):
     if os.path.isdir(name):
         os.rmdir(name)
     else:
         os.remove(name)
+    print(f'File {name} is deleted')
 
+#Копируем файл/папку
 def copy_file(name, new_name):
     if os.path.isdir(name):
         try:
@@ -27,6 +40,22 @@ def copy_file(name, new_name):
             print('This folder already exists')
     else:
         sh.copy(name, new_name)
+    print(f'File {name} is copied with name {new_name}')
+
+#Переименовываем файл/папку
+def rename_file(name, new_name):
+    os.rename(name, new_name)
+    print(f'File {name} is renamed as {new_name}')
+
+def move_directorty(name):
+    os.chdir(name)
+    print('Directory is changed')
+
+def return_indirectory():
+    pass
+
+
+
 
 if __name__ == '__main__':
     create_file('text.txt')
